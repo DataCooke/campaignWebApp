5e4ea1f2825592122dab36d87794b6b7e3ed622f
Jon Cooke <cooke.jon.mba@gmail.com>
2018-07-09 09:15:45 -0600

    Add files via upload

--- server.R
+++ server.R
@@ -4,7 +4,7 @@ library
 
 function(input, output) {
   
-  all <- reactive(paste(req(input$URL), if (input$source == 300) {""} else input$source, if (input$medium == 300) {""} else (input$medum), if (input$campaign > 0) {paste("&utm_campaign=", input$campaign, sep = "")}, if (input$market == 300) {""} else input$market, input$content, sep = ""))
+  all <- reactive(paste(req(input$URL), if (input$source == 300) {""} else input$source, if (input$medium == 300) {""} else (input$medium), if (input$campaign > 0) {paste("&utm_campaign=", input$campaign, sep = "")}, if (input$market == 300) {""} else input$market, input$content, sep = ""))
   output$all <- renderText(paste(all(), sep = ""))
   #output$all <- renderText(paste(req(input$URL), input$source, input$select, if (input$variable > 0) {"&utm_campaign="}, input$variable, input$market, sep = ""))
   
